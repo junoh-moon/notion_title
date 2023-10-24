19d95f6a654522bf3332fdeba77ada02a20bdefd
Junoh Moon <mjo970625@gmail.com>
2023-10-24 15:57:21 +0900

    Refactor main.py to improve logging and argument parsing
    - Import the `log` module for logging
    - Add a logger instance to the main.py file
    - Change argument parsing to include logging information
    - Adjust the argument assignments to account for changes in argument parsing
    
    Changes:
    - Import the `log` module
    - Add `logger = log.init()` to initialize the logger
    - Change argument parsing to include additional logging
    - Adjust assignment of `args` variable to use the correct parsed arguments

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import argparse
 
+import log
 import title
 import updater
+
+logger = log.init()
 
 
 def main(args):
@@ -18,12 +21,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     sub = parser.add_mutually_exclusive_group(required=True)
+
     sub.add_argument("--generate", action="store_true")
     sub.add_argument("--watch", action="store_true")
     sub.add_argument("--update", action="store_true")
 
-    args = parser.parse_args()
-    if args.generate:
+    raw_args = parser.parse_known_args()
+    logger.info(raw_args)
+    if raw_args[0].generate:
         title.prepare_args(parser)
         args = parser.parse_args()
+    else:
+        args = raw_args[0]
     main(args)
