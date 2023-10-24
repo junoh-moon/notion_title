fe25be1aa0ecce8fe4f3e0e6049eb4396e683e30
Junoh Moon <mjo970625@gmail.com>
2023-10-24 22:10:48 +0900

    Update Updater class to support different templates for each week.
    - Added new variable `NEXT_WEEK_TEMPLATE` to store the template for the next week.
    - Modified `_detect_changes` method to determine the new title based on the current title of the child pages.
    - Modified `_update_title` method to accept the new title as a parameter and update the child page's title accordingly.

--- updater.py
+++ updater.py
@@ -1,5 +1,8 @@
 import os
-from datetime import datetime
+from datetime import (
+    datetime,
+    timedelta,
+)
 from time import sleep
 
 from dotenv import load_dotenv
@@ -19,6 +22,7 @@ twenty_twenty_three_id = "b1a820decce443c795f7157f77bd7698"
 
 class Updater:
     TEMPLATE_NAME = "[Template] Week N, Month, Year (MM/DD—MM/DD) (1)"
+    NEXT_WEEK_TEMPLATE = "{{next}}"
     INTERVAL = int(os.environ.get("INTERVAL_IN_SEC", "60"))
 
     def __init__(self, watch_page_id: str) -> None:
@@ -39,31 +43,40 @@ class Updater:
 
     def update(self):
         page_ids = self._detect_changes(self.watch_page_id)
-        for page_id in page_ids:
-            self._update_title(page_id)
+        for page_id, new_title in page_ids.items():
+            self._update_title(page_id, new_title)
 
     def _detect_changes(self, parent_child_id: str):
-        page_ids = []
+        page_title_mapping = dict()
         children = self.notion.blocks.children.list(parent_child_id)
         for child in children["results"]:  # type: ignore
             logger.debug(child)
             if child["type"] != "child_page":
                 continue
-            if child["child_page"]["title"] != self.TEMPLATE_NAME:
+            title: str = child["child_page"]["title"]
+            new_title = {
+                True: None,
+                title == self.TEMPLATE_NAME: get_title(datetime.now().date()),
+                title.lower()
+                == self.NEXT_WEEK_TEMPLATE: get_title(
+                    (datetime.now() + timedelta(days=7)).date()
+                ),
+            }[True]
+            if new_title is None:
                 continue
             child_id = child["id"]
-            page_ids.append(child_id)
+            page_title_mapping[child_id] = new_title
 
-        logger.info(f"{page_ids=}")
-        return page_ids
+        logger.info(f"{page_title_mapping=}")
+        return page_title_mapping
 
-    def _update_title(self, page_id: str):
+    def _update_title(self, page_id: str, new_title: str):
         body = self.notion.pages.retrieve(page_id)
         logger.info(f"Previous: {body}")
 
         body["properties"]["title"]["title"][0]["text"][
             "content"
-        ] = self.new_title  # type: ignore
+        ] = new_title  # type: ignore
 
         resp = self.notion.pages.update(page_id, **body)  # type: ignore
         logger.info(f"After: {resp}")
